e11abe9c1964780bd1b34a1670bd8c0a973a317e
Thomas Pfund (U116423) <thomas.pfund@mobi.ch>
2016-05-12 21:06:33 +0200

    Update automatique des stats toutes les 2 secondes

--- src/main/webapp/app/src/users/AdminController.js
+++ src/main/webapp/app/src/users/AdminController.js
@@ -3,43 +3,54 @@
   angular
        .module('users')
        .controller('AdminController', [
-          'adminService', '$scope', AdminController
+          'adminService', '$scope', '$interval', '$rootScope', AdminController
        ]);
   ;
 
-  function AdminController(adminService, $scope) {
+  function AdminController(adminService, $scope, $interval, $rootScope) {
     var self = this;
       self.searchStatsByNameOrder = '_id';
       self.searchStatsByTypeOrder = '_id';
-      
-    adminService.getSearchStatsByType().then(function(response) {
-        self.searchStatsByType = response.data;
-        $scope.labelsPie = [];
-        $scope.dataPie = [];
 
-        var searchStatsByType = response.data;
-        for (var i = 0; i < searchStatsByType.length; i++) {
-            var item = searchStatsByType[i];
-            $scope.labelsPie.push(item._id.toUpperCase());
-            $scope.dataPie.push(item.count);
-        }
-    });
+      var updateData = function() {
+          adminService.getSearchStatsByType().then(function (response) {
+              self.searchStatsByType = response.data;
+              $scope.labelsPie = [];
+              $scope.dataPie = [];
 
-      adminService.getSearchStatsByName().then(function(response) {
-          self.searchStatsByName = response.data;
-        $scope.labelsBarChart = [];
-        $scope.seriesBarChart = ["Nombre de recherches "];
-        $scope.dataBarChart = [];
+              var searchStatsByType = response.data;
+              for (var i = 0; i < searchStatsByType.length; i++) {
+                  var item = searchStatsByType[i];
+                  $scope.labelsPie.push(item._id.toUpperCase());
+                  $scope.dataPie.push(item.count);
+              }
+          });
 
-          var searchStatsByName = response.data;
-          var dataTable = [];
-          for (var i = 0; i < searchStatsByName.length; i++) {
-              var item = searchStatsByName[i];
-              $scope.labelsBarChart.push(item._id);
-              dataTable.push(item.count);
-          }
-          $scope.dataBarChart.push(dataTable);
+          adminService.getSearchStatsByName().then(function (response) {
+              self.searchStatsByName = response.data;
+              $scope.labelsBarChart = [];
+              $scope.seriesBarChart = ["Nombre de recherches "];
+              $scope.dataBarChart = [];
+
+              var searchStatsByName = response.data;
+              var dataTable = [];
+              for (var i = 0; i < searchStatsByName.length; i++) {
+                  var item = searchStatsByName[i];
+                  $scope.labelsBarChart.push(item._id);
+                  dataTable.push(item.count);
+              }
+              $scope.dataBarChart.push(dataTable);
+          });
+      }
+      updateData();
+
+      self.myInterval = $interval(updateData, 2000);
+
+      var dereg = $rootScope.$on('$locationChangeSuccess', function() {
+          $interval.cancel(self.myInterval);
+          dereg();
       });
+
   }
 
 })();
